1d9bbe9edaf44a4fb96b428604a4b2992234e662
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2023-12-04 11:43:08 +0530

    added CTA Section in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,18 @@
 import { ModeToggle } from "@/components/darkmodetoggle";
+import CTASection from "@/components/modules/CTASection";
+import HeroSection from "@/components/modules/HeroSection";
+import OurIntro from "@/components/modules/OurIntro";
+import Testimonials from "@/components/modules/Testimonials";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  justify-around p-24">
-      <div>
-        <h1 className="text-6xl font-bold text-center">
-          Welcome to{" "}
-          <Link href="/" className="text-blue-600">
-            Smart Donkey
-          </Link>
-        </h1>
-        <p className="text-xl text-center py-4 text-gray-500 font-mono ">
-          An Educational Platform for Gen-z, We teach things that actually
-          matters
-        </p>
-      </div>
-
-      {/* <div className="text-center">
-        <ModeToggle />
-      </div> */}
-      <p className="text-2xl text-center">
-        Get started by choosing a course{" "}
-        <code className="bg-gray-100 p-2 rounded-md text-lg font-mono">
-          pages/index.js
-        </code>
-      </p>
+    <main>
+      <HeroSection />
+      <OurIntro />
+      {/* <Testimonials /> */}
+      <CTASection />
     </main>
   );
 }
