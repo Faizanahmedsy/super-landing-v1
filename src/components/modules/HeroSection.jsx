9791270e0df7dce5a82eba5f343ec14347058cac
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2023-12-06 14:10:38 +0530

    style: added a courses button in hero section

--- src/components/modules/HeroSection.jsx
+++ src/components/modules/HeroSection.jsx
@@ -1,31 +1,33 @@
 import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
 
 export default function HeroSection() {
   return (
     <section className="flex min-h-screen flex-col  justify-around p-24">
       <div>
-        <h1 className="text-6xl font-bold text-center">
+        <h1 className="text-6xl font-bold text-center tracking-tight">
           Welcome to{" "}
           <Link href="/" className="text-blue-600">
             Smart Donkey
           </Link>
         </h1>
-        <p className="text-xl text-center py-4 text-gray-500 font-mono ">
-          An Educational Platform for Gen-z, We teach things that actually
-          matters
+        <p className="text-xl text-center py-2 text-muted-foreground font-mono">
+          <br />
+          An Educational Platform for Gen-z, Which teaches things that <br />
+          actually matters in life.
         </p>
       </div>
 
-      {/* <div className="text-center">
-        <ModeToggle />
-      </div> */}
-      <p className="text-2xl text-center">
+      <div className="text-2xl text-center">
         Get started by choosing a course{" "}
-        <code className="bg-gray-100 p-2 rounded-md text-lg font-mono">
-          pages/index.js
-        </code>
-      </p>
+        {/* <code className="bg-gray-100 p-2 rounded-md text-lg font-mono">
+          here
+        </code> */}
+        <Button className="mx-auto" href="/courses">
+          Courses
+        </Button>
+      </div>
     </section>
   );
 }
